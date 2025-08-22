d0ad70c3c3f782f15a04f5090c7e79031a1f42b3
wenxi.goh <wenxi.goh@gmail.com>
2025-08-22 02:25:04 +0000

    added logic to get a python dictionary of taxi locations

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import requests
 import json
 import folium
+from pyodide.ffi import JsProxy
+def get_taxi_json(event=None):
+    response = requests.get("https://raw.githubusercontent.com/Wen-Xi-Goh/taxi-locations/refs/heads/main/taxi-data.json")
+    get_taxi_dict(response.text)
+    return response.text
+def get_taxi_dict(jsonText = None):
+    if jsonText==None:
+        jsonText=get_taxi_json()
+    # Convert the JsProxy object to a Python string
+    #print("Content of jsonText:", jsonText)
+    responsedict=json.loads(jsonText)
+    print(responsedict)
